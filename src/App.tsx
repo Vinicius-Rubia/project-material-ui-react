410c257b5789ff0bbb6eae767681408f802ff1f9
vinicius-rubia <vinicius.rubia@negocieonline.com.br>
2023-01-05 14:39:04 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-function App() {
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
+function App() {
   return (
-    <div className="App">
-      MATERIAL UI      
-    </div>
-  )
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
